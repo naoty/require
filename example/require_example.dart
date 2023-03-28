204a084e5a654f2a194cd08a5fc0ab04cecac823
Naoto Kaneko <naoty.k@gmail.com>
2023-03-28 23:42:10 +0900

    Delete boilerplate codes

--- example/require_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:require/require.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- lib/require.dart
+++ lib/require.dart
@@ -1,8 +1,1 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library require;
-
-export 'src/require_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
